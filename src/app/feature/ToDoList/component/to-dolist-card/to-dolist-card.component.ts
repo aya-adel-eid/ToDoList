d42b9ec131baf313b86a8b0d48968343e08166d6
Aya ِAdel <ayaadelyahoo66@gmail.com>
2026-08-21 02:58:26 +0300

    style: add taskCard ui

---
+++ src/app/feature/ToDoList/component/to-dolist-card/to-dolist-card.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-to-dolist-card',
+  imports: [],
+  templateUrl: './to-dolist-card.component.html',
+  styleUrl: './to-dolist-card.component.css',
+})
+export class ToDolistCardComponent {
+
+}
--- src/app/feature/ToDoList/pages/to-do-list-page/to-do-list-page.component.ts
+++ src/app/feature/ToDoList/pages/to-do-list-page/to-do-list-page.component.ts
@@ -1,9 +1,10 @@
 import { Component, inject, signal } from '@angular/core';
 import { ToDoListService } from '../../to-do-list.service';
+import { ToDolistCardComponent } from '../../component/to-dolist-card/to-dolist-card.component';
 
 @Component({
   selector: 'app-to-do-list-page',
-  imports: [],
+  imports: [ToDolistCardComponent],
   templateUrl: './to-do-list-page.component.html',
   styleUrl: './to-do-list-page.component.css',
 })
