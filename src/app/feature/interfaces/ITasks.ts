29859421bb1b61fdf742285761ad6ba1451fa277
Aya ِAdel <ayaadelyahoo66@gmail.com>
2026-08-21 03:01:39 +0300

    feat: make interface for tasks

---
+++ src/app/feature/interfaces/ITasks.ts
@@ -0,0 +1,14 @@
+export interface ITasks {
+  success: boolean;
+  data: Task[];
+}
+
+export interface Task {
+  id: number;
+  title: string;
+  description?: string;
+  is_completed: boolean;
+  due_at?: string;
+  created_at: string;
+  updated_at: string;
+}
